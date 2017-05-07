e672c6c781f6fe3450ad0aacf89516b3ef236145
Alex Vanyo <vanyoalex@gmail.com>
2017-05-07 00:23:22 -0500

    Initial commit

---
+++ app/src/main/java/com/alexvanyo/simplicitypluscompanion/MainActivity.java
@@ -0,0 +1,11 @@
+package com.alexvanyo.simplicitypluscompanion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
